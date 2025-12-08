d4ed110137342aea0e71a3e7f33721f1d10c6f5b
Abdullah Sajid <abdullahsajid320@gmail.com>
2025-12-09 02:23:07 +0500

    this is the one more code

---
+++ Day-1/subtract.py
@@ -0,0 +1,12 @@
+# this program allows u to enter two numbers and it will return their sum
+def additon(*arg):
+    total=0
+    for i in arg:
+        total-=i
+    return total
+    
+
+
+
+a= additon(2,8)
+print(a)

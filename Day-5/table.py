7b1f4522e506fe1ad0dfe8389a38235c27e03af2
Abdullah Sajid <abdullahsajid320@gmail.com>
2025-12-10 18:21:05 +0500

    This is the table maker

---
+++ Day-5/table.py
@@ -0,0 +1,14 @@
+# This program give us the full table by inputing the number 
+def table(num):
+    for i in range(1,11):
+        print(f"{num} x {i} = {num*i}")
+
+
+while True:
+    try:
+        a= int(input("Enter the number: "))
+        break
+    except:
+        print("Invalid Input! Please enter numbers only!!")
+ 
+table(a)

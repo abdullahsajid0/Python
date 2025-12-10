1002b493066bd5eeccbf14b2a0563ecbf7c0c72b
Abdullah Sajid <abdullahsajid320@gmail.com>
2025-12-10 20:13:02 +0500

    This is the hollow trinagle  shapes for stars

--- Day-5/patterns.py
+++ Day-5/patterns.py
@@ -72,6 +72,20 @@ def hollowSquare(num):
             else:
                 print(" ", end=" ")
         print()
+def hollowTriangle(num):
+    print("This is the This is the Hollow Triangle\n")
+    for i in range(1, num + 1):
+        # Print leading spaces
+        for j in range(num - i):
+            print(" ", end=" ")
+
+        # Print stars and spaces
+        for k in range(1, 2 * i):
+            if k == 1 or k == 2 * i - 1 or i == num:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
 
 
 while True:
@@ -89,3 +103,4 @@ centerstar(a)
 invcenterstar(a)
 diamondshape(a)
 hollowSquare(a)
+hollowTriangle(a)
